9a36ff87a439eff00766d8b5d9fcc8a6772184a6
alauters <austin-lauters@uiowa.edu>
2021-12-04 18:14:29 -0600

    led functions test

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import RPi.GPIO as GPIO
-
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(26, GPIO.OUT)
-
-
+import time
 
 def main():
-    print("Hello World")
+    # sets board up to use gpio numbers
+    GPIO.setmode(GPIO.BCM)
+    led = 26
+    GPIO.setup(led, GPIO.OUT)
 
+    for i in range(1,5):
+        GPIO.setmode(led, GPIO.HIGH)
+        time.sleep(5)
+        GPIO.setmode(led, GPIO.LOW)
+        time.sleep(5)
+
+    GPIO.cleanup()
 
 
 if __name__ == "__main__":
